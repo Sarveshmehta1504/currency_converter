e83162dc4a9af9fdd552a9f75b5834de8228af93
Sarveshmehta1504 <sarveshmehta1504@gmail.com>
2025-07-03 05:48:38 +0530

    Refactor main application structure and update Flutter window initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,39 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text(
-    "Hello, World!",
-    textDirection: TextDirection.rtl,
-  ));
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sarvesh Mehta",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black, 
+                ),
+              ),
+              TextField(
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: "Please enter the text here"
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
